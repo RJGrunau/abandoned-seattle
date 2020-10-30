16b5f2ab6806c2f285f3bd74ddaadcf32dbda22d
Robert Grunau <robertgrunau@gmail.com>
2020-10-29 21:12:56 -0700

    starting styles

--- components/index-page/hero-block/heroBlock.jsx
+++ components/index-page/hero-block/heroBlock.jsx
@@ -2,8 +2,11 @@ import './heroBlock.module.scss'
 import { Image } from 'react-datocms';
 const HeroBlock = ({image}) => {
     return (  
-        <section>
-            <Image data={image}/>
+        <section className="hero">
+            <div className="wrapper hero-grid">
+
+                <Image className="hero-image" data={image}/>
+            </div>
         </section>
     );
 }
