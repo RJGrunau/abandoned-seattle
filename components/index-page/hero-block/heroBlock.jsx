9fc5a30f80486ab312aa0db426023e5c0aec3fe7
Robert Grunau <robertgrunau@gmail.com>
2020-10-30 20:40:35 -0700

    troubleshooting app

--- components/index-page/hero-block/heroBlock.jsx
+++ components/index-page/hero-block/heroBlock.jsx
@@ -1,15 +1,18 @@
 import { Image } from 'react-datocms';
+import styles from "./heroBlock.module.scss"
 const HeroBlock = ({image}) => {
     return (  
-        <section className="hero">
-            <div className="backing">
-                <Image className="hero-image" className="hero-image" data={image} style={{objectFit: "cover"}}/>
-                <div className="page-text">
-                    <h2>Abandoned Seattle:</h2>
-                    <p>A photographic essay of Seattle's abandoned buildings before they become appartments</p>
+        <div className="wrapper">
+            <section className={styles.hero }>
+                <div className={styles.backing}>
+                    <Image cdata={image} style={{objectFit: "cover"}}/>
+                    <div>
+                        <h2>Abandoned Seattle:</h2>
+                        <p>A photographic essay of Seattle's abandoned buildings before they become appartments</p>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
     );
 }
  
