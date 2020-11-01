a58134af4ef63a81622d7f2e71fa6d555465c317
Robert Grunau <robertgrunau@gmail.com>
2020-11-01 14:40:31 -0800

    about block with content

--- components/index-page/about-block/aboutBlock.jsx
+++ components/index-page/about-block/aboutBlock.jsx
@@ -1,13 +1,15 @@
-import Image from "react-datocms"
+import {Image} from "react-datocms"
 import styles from "./aboutBlock.module.scss"
 
 
 const AboutBLock = ({img, text}) => {
     return ( 
         <section className={styles.aboutSection}>
-            <Image data={img} style={{objectFit: "cover"}} explicitWidth="100%"/>
             <div>
-                {text}
+                <Image data={img} style={{objectFit: "cover"}} explicitWidth="100%"/>
+            </div>
+            <div>
+                <p>{text}</p>
             </div>
         </section>
      );
