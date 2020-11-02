be5cc5552c55a873535ec68298479568c1fa3d8a
Robert Grunau <robertgrunau@gmail.com>
2020-11-01 17:11:04 -0800

    troubleshooting

--- components/photo-stream/photoStream.jsx
+++ components/photo-stream/photoStream.jsx
@@ -5,18 +5,20 @@ import PhotoCard from './photo-card/photoCard'
 
 const PHOTOSTREAM_QUERY = `
     query PhotoStream {
-        stream{
-                responsiveImage(imgixParams: { fit: max, w: 960, h: 672, auto: format }) {
-                srcSet
-                webpSrcSet
-                sizes
-                src
-                width
-                height
-                aspectRatio
-                alt
-                title
-                base64
+        photoStream{
+            stream{
+                    responsiveImage(imgixParams: { fit: max, w: 960, h: 672, auto: format }) {
+                    srcSet
+                    webpSrcSet
+                    sizes
+                    src
+                    width
+                    height
+                    aspectRatio
+                    alt
+                    title
+                    base64
+                }
             }
         }
     }
@@ -33,11 +35,11 @@ export async function getStaticProps(){
     }
 }
 const PhotoStream = ({data}) => {
-    let testPhoto = data.photoStream.stream[0]
+    console.log(data);
     return ( 
         <div className="wrapper">
             <section className={styles.stream}>
-                <PhotoCard img={testPhoto} alt={testPhoto.alt} caption={testPhoto.title}/>
+                {/* <PhotoCard img={testPhoto} alt={testPhoto.alt} caption={testPhoto.title}/> */}
             </section>
         </div>
      );
