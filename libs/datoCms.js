afb35707dd6a856c67156aac87b756c35a9412fa
Robert Grunau <robertgrunau@gmail.com>
2020-11-01 19:33:06 -0800

    slightly better error handling

--- libs/datoCms.js
+++ libs/datoCms.js
@@ -2,11 +2,18 @@ import { GraphQLClient } from 'graphql-request'
 export function request ({query, variables, preview}) {
     const endpoint = preview ? `https://graphql.datocms.com/preview`
     : `https://graphql.datocms.com/`
+    try {
+        const client = new GraphQLClient(endpoint, {
+            headers: {
+                authorization: `Bearer ${process.env.DATOCMS_API_TOKEN}`
+            }
+        })
+        return client.request(query, variables)
+        
+    } catch (error) {
+        console.log('request error: ',error);
+    }
+   
 
-    const client = new GraphQLClient(endpoint, {
-        headers: {
-            authorization: `Bearer ${process.env.DATOCMS_API_TOKEN}`
-        }
-    })
-    return client.request(query, variables)
+    
 }
