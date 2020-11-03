43d0b81edc5888a959539136599633351eada8fa
Robert Grunau <robertgrunau@gmail.com>
2020-11-03 14:45:17 -0800

    got some data for about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,9 +1,51 @@
 const { default: GlobalLayout } = require("../components/layout-components/global-layout/layout")
 
-const AboutPage = () => {
+import { request } from '../libs/datoCms'
+
+
+const ABOUT_QUERY = `
+    query AboutPage{
+        page(filter: {title: {eq: "Abandoned Seattle - Home"}}){
+            title
+            pageText
+            assets {
+                    responsiveImage(imgixParams: { fit: max, w: 960, h: 672, auto: format }) {
+                    srcSet
+                    webpSrcSet
+                    sizes
+                    src
+                    width
+                    height
+                    aspectRatio
+                    alt
+                    title
+                    base64
+              }
+        }
+    }
+}
+`
+export async function getStaticProps(){
+    const data = await request({ 
+        query: ABOUT_QUERY,
+        variables: {limit: 1}
+       })
+    return{
+        props: {
+            data
+        }
+    }
+}
+
+const AboutPage = ({data}) => {
     return ( 
         <GlobalLayout>
-            <div>about</div> 
+            <div className="wrapper">
+                <article>
+                    <h1>{data.page.title}</h1>
+                    about
+                </article>
+            </div> 
         </GlobalLayout>
     );
 }
