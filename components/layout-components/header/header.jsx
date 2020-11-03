b212d31475e19304ba4588a555033dc9a5de74ba
Robert Grunau <robertgrunau@gmail.com>
2020-11-03 14:06:30 -0800

    about link

--- components/layout-components/header/header.jsx
+++ components/layout-components/header/header.jsx
@@ -1,11 +1,20 @@
 import styles from './header.module.scss'
-
+import Link from 'next/link'
 
 const GlobalHeader = () => {
     return ( 
         <header className={styles.header}>
             <div className={styles.headerContainer}>
                 <h1>Abandoned Seattle</h1>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link href="/about">
+                                <a>About</a>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </header>
      );
