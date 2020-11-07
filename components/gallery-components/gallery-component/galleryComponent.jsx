2f225529b57f3415b3138c982834eeab38a172dc
Robert Grunau <robertgrunau@gmail.com>
2020-11-06 22:06:24 -0800

    wtf

--- components/gallery-components/gallery-component/galleryComponent.jsx
+++ components/gallery-components/gallery-component/galleryComponent.jsx
@@ -1,10 +1,17 @@
+import GalleryPicture from '../gallery-picture/galleryPictureComponent';
 import styles from './galleryComponent.module.scss'
 
 const GalleryComponent = ({gallery}) => {
+    const photos = gallery.gallery
+    console.log(photos);
     return ( 
         <div className={styles.galleryWrapper}>
-            <section className={styles.gallery}>
-                hi
+            <section className={styles.galleryContainer}>
+                {
+                    photos.map(pic => {
+                        <GalleryPicture img={pic} />
+                    })
+                }
             </section>
         </div>
      );
--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -1,34 +1,53 @@
+import { request } from "../libs/datoCms"
 import React, {useEffect, useState} from 'react'
-import {flikrHelper} from '../libs/flikrHelper';
 import GalleryComponent from "../components/gallery-components/gallery-component/galleryComponent";
+import GlobalLayout from "../components/layout-components/global-layout/layout";
 
+const GALLERY_QUERY = `
+    query GalleryPage{
+        photoStream {
+            stream {
+                responsiveImage(imgixParams: { fit: max, w: 960, h: 672, auto: format }) {
+                srcSet
+                webpSrcSet
+                sizes
+                src
+                width
+                height
+                aspectRatio
+                alt
+                title
+                base64
+              }
+            }
+        }
+    }
+`
 
-const GalleryPage = (photos) => {
-    const [ gallery, setGallery ] = useState(null)
-    // const [ loading, setLoading ] = useState({loading: false})
-    // const gallery = photos.gallery.photoset.photo
+const GalleryPage = ({data}) => {
+    const [gallery, setGallery] = useState(null);
+
     useEffect(() => {
-        const photoGallery = photos.gallery.photoset.photo
-        setGallery({gallery: photoGallery})
-    }, []);
+        const photos = data.photoStream.stream
 
+        setGallery({gallery: photos})
+    }, [])
     return ( 
-        <GalleryComponent/>
+        <GlobalLayout>
+            <GalleryComponent gallery={gallery}/>
+        </GlobalLayout>
      );
 }
  
-GalleryPage.getInitialProps = async (ctx) => {
-    try {
-        const response = await fetch(`https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${process.env.FLIKR_API}&photoset_id=${process.env.ABAND_ALBUM}&user_id=190565882%40N05&extras=url_s%2Curl_m%2Curl_o&media=&format=json&nojsoncallback=1`)
-        const json = await response.json()
-        
-        return{
-            photos: json
+export async function getStaticProps(){
+    const data = await request({
+        query: GALLERY_QUERY
+    })
+
+    return {
+        props: {
+            data
         }
-
-
-    } catch (error) {
-        console.error('flikr error: ', error);
     }
 }
 export default GalleryPage;
