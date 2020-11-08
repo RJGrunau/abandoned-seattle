ee8cdfe7f787069e9fc1d08f3070e9741e92eb59
Robert Grunau <robertgrunau@gmail.com>
2020-11-08 13:50:53 -0800

    increment works with no stop

--- components/gallery-components/gallery-component/galleryComponent.jsx
+++ components/gallery-components/gallery-component/galleryComponent.jsx
@@ -1,15 +1,25 @@
+import React from 'react'
 import styles from './galleryComponent.module.scss'
 import GalleryPicture from '../gallery-picture/galleryPictureComponent'
 
 const GalleryComponent = ({title,assets}) => {
     const images = assets
+    const [fImage, setFImage] = React.useState(0)
+    const incrementImage = () =>{
+        let numberOfPhotos = images.lenghth
+        const stopper = 1
+        numberOfPhotos = numberOfPhotos - stopper
+        let slideIndex = fImage
+        slideIndex ++ 
+        setFImage(slideIndex)
+    }
     return ( 
         <div className={styles.galleryWrapper}>
                 
             <section className={styles.galleryContainer}>  
-                <GalleryPicture img={images[0]} />
+                <GalleryPicture img={images[fImage]} />
             <div className={styles.galleryNav}>
-                <div>back</div> <div>next</div>
+                <div>back</div> <div onClick={incrementImage}>next</div>
             </div>
             </section>
         </div>
