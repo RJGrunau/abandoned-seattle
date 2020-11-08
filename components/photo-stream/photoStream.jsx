aa0ca4d8e87ab1bb50091f9695279aea53243d26
Robert Grunau <robertgrunau@gmail.com>
2020-11-08 14:40:14 -0800

    forgotten

--- components/photo-stream/photoStream.jsx
+++ components/photo-stream/photoStream.jsx
@@ -1,14 +1,23 @@
+import Link from 'next/link'
 import styles from './photoStream.module.scss'
 import PhotoCard from './photo-card/photoCard'
-
 
 const PhotoStream = ({photos}) => {
     return ( 
         <div className="wrapper">
             <section className={styles.stream}>
-                {photos.map((photo,i) => (
-                    <PhotoCard key={i} img={photo}/>
-                ))}
+                <div>
+                    {photos.map((photo,i) => (
+                        <PhotoCard key={i} img={photo}/>
+                    ))}
+                </div>
+                <div className={styles.moreButton}>
+                    <Link href="/gallery">
+                        <a>
+                            More of Abandoned Seattle
+                        </a>
+                    </Link>
+                </div>
             </section>
         </div>
      );
