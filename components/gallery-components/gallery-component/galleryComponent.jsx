a0bad14b58cab7795063d81b2e025c642add8bb4
Robert Grunau <robertgrunau@gmail.com>
2020-11-08 14:08:29 -0800

    back to this

--- components/gallery-components/gallery-component/galleryComponent.jsx
+++ components/gallery-components/gallery-component/galleryComponent.jsx
@@ -4,23 +4,16 @@ import GalleryPicture from '../gallery-picture/galleryPictureComponent'
 
 const GalleryComponent = ({title,assets}) => {
     const images = assets
-    const [fImage, setFImage] = React.useState(0)
-    const incrementImage = () =>{
-        let numberOfPhotos = images.lenghth
-        const stopper = 1
-        numberOfPhotos = numberOfPhotos - stopper
-        let slideIndex = fImage
-        slideIndex ++ 
-        setFImage(slideIndex)
-    }
+    
+
     return ( 
         <div className={styles.galleryWrapper}>
                 
             <section className={styles.galleryContainer}>  
-                <GalleryPicture img={images[fImage]} />
-            <div className={styles.galleryNav}>
-                <div>back</div> <div onClick={incrementImage}>next</div>
-            </div>
+                {images.map((img,i) => (
+                    <GalleryPicture key={i} img={img} />
+                ))}
+            
             </section>
         </div>
      );
