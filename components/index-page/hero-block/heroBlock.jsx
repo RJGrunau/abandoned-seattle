711f0c1d913fe2d2cef1c0efa1730950131dff72
Robert Grunau <robertgrunau@gmail.com>
2020-11-09 21:09:44 -0800

    more styles

--- components/index-page/hero-block/heroBlock.jsx
+++ components/index-page/hero-block/heroBlock.jsx
@@ -9,7 +9,7 @@ const HeroBlock = ({image}) => {
                         <Image data={image} style={{objectFit: "cover"}} explicitWidth="100%"/>
                     </div>
                     <div className={styles.heroText}>
-                        <h4>A photographic essay of Seattle's abandoned buildings before they become appartments</h4>
+                        <h4>A photographic essay of Seattle's abandoned buildings before they become condos</h4>
                     </div>
                 </div>
             </section>
